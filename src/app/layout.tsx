e30798f973e507efc160cbd56ece5647c39a83da
Vcciz <viniciosnorges2018@gmail.com>
2026-03-05 23:07:20 -0300

    Set text logo as site icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "./globals.css";
 import Header from "@/src/components/Header";
 import NewsletterPopup from "@/src/components/NewsletterPopup";
+import type { Metadata } from "next";
+import logoTexto from "@/src/public/assets/imagens/logo s texxto.png";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: logoTexto.src,
+  },
+};
 export default function RootLayout({
   children,
 }: {
